a5d40311da74e6d85e65fc621f6382cb195a823f
LogicalInsanity <idkwtw223@gmail.com>
2021-01-26 10:59:29 +0100

    2.1 done

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,40 +1,36 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.list.IntArrayStack;
+
 public class DefaultCountingOutRhymer {
+	private IntArrayStack intArrayStack;
 
-	public final int MAX_SIZE = 12;
-	public final int FULL = MAX_SIZE-1;
-	public final int INITIAL = -1;
-	public final int DEFAULT = -1;
-
-	private final int[] numbers = new int[MAX_SIZE];
-	private int total = INITIAL;
-
+	public DefaultCountingOutRhymer(){
+		intArrayStack = new IntArrayStack();
+	}
+	public DefaultCountingOutRhymer(IntArrayStack i){
+		intArrayStack = i;
+	}
 	public void countIn(int in) {
-		if (!isFull())
-			numbers[++total] = in;
+		intArrayStack.countIn(in);
 	}
 
 	public boolean callCheck() {
-		return total == INITIAL;
+		return intArrayStack.callCheck();
 	}
 
 	public boolean isFull() {
-		return total == FULL;
+		return intArrayStack.isFull();
 	}
 
 	protected int peekaboo() {
-		if (callCheck())
-			return DEFAULT;
-		return numbers[total];
+		return intArrayStack.peekaboo();
 	}
 
 	public int countOut() {
-		if (callCheck())
-			return DEFAULT;
-		return numbers[total--];
+		return intArrayStack.countOut();
 	}
 	public int getTotal() {
-		return total;
+		return intArrayStack.getTotal();
 	}
 }
---
+++ src/main/java/edu/kis/vh/nursery/list/IntArrayStack.java
@@ -0,0 +1,40 @@
+package edu.kis.vh.nursery.list;
+
+public class IntArrayStack {
+
+	public final int MAX_SIZE = 12;
+	public final int FULL = MAX_SIZE-1;
+	public final int INITIAL = -1;
+	public final int DEFAULT = -1;
+
+	private final int[] numbers = new int[MAX_SIZE];
+	private int total = INITIAL;
+
+	public void countIn(int in) {
+		if (!isFull())
+			numbers[++total] = in;
+	}
+
+	public boolean callCheck() {
+		return total == INITIAL;
+	}
+
+	public boolean isFull() {
+		return total == FULL;
+	}
+
+	public int peekaboo() {
+		if (callCheck())
+			return DEFAULT;
+		return numbers[total];
+	}
+
+	public int countOut() {
+		if (callCheck())
+			return DEFAULT;
+		return numbers[total--];
+	}
+	public int getTotal() {
+		return total;
+	}
+}
