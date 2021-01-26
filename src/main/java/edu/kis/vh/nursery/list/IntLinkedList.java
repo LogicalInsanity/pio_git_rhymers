cc389b10d62917a103b8e599634ee70e5efcc368
LogicalInsanity <idkwtw223@gmail.com>
2021-01-26 11:07:58 +0100

    merge request fixes

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -25,13 +25,13 @@ public class IntLinkedList {
 	public int top() {
 		if (isEmpty())
 			return RETURN_VALUE;
-		return last.value;
+		return last.getValue();
 	}
 
 	public int pop() {
 		if (isEmpty())
 			return RETURN_VALUE;
-		int ret = last.value;
+		int ret = last.getValue();
 		last = last.prev;
 		return ret;
 	}
--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,10 +2,13 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-	protected final int value;
+	private final int value;
 	protected Node prev, next;
 
 	public Node(int i) {
 		value = i;
 	}	
+	public int getValue() {
+		return value;
+	}
 }
